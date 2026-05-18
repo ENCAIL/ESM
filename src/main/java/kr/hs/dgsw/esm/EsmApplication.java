2bde18748a5c41eef044a78a4a670b6b25a6f08d
encail <siwon0876@gmail.com>
2026-05-18 11:29:08 +0900

    👍

---
+++ src/main/java/kr/hs/dgsw/esm/EsmApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.esm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/hs/dgsw/esm/EsmApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.esm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
