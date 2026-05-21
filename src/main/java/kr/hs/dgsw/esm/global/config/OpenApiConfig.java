77327fa385c1fd40c25eaba3c3b9d269387098ea
encail <siwon0876@gmail.com>
2026-05-21 10:54:48 +0900

    👍

---
+++ src/main/java/kr/hs/dgsw/esm/global/config/OpenApiConfig.java
@@ -0,0 +1,33 @@
+package kr.hs.dgsw.esm.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        String securitySchemeName = "bearerAuth";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securitySchemeName);
+        Components components = new Components()
+                .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                        .name(securitySchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("ESM API Documentation")
+                        .description("전자상거래 쇼핑몰 플랫폼 API 명세서")
+                        .version("0.0.1"))
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
